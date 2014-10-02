84846379bac1b866a1ad86cc068014b85b9f5473
Jason Dobry <jason.dobry@gmail.com>
2014-10-02 16:02:40 -0600

    Example update.

--- example/example.js
+++ example/example.js
@@ -2,44 +2,33 @@
   angular.module('localforage-example', [])
     .factory('store', function () {
       var store = new JSData.DS();
+
       store.registerAdapter('localforage', new DSLocalForageAdapter(), { default: true });
+
       return store;
     })
     .factory('User', function (store) {
       return store.defineResource('user');
     })
-    .controller('localforageCtrl', function ($scope, $timeout, User) {
+    .controller('localforageCtrl', function ($scope, User) {
       var lfCtrl = this;
 
-      User.findAll().then(function () {
+      User.findAll().then(function (users) {
+        $scope.users = users;
         $scope.$apply();
       });
 
       $scope.add = function (user) {
-        $scope.creating = true;
         User.create(user).then(function () {
-          $scope.creating = false;
           lfCtrl.name = '';
-          $timeout();
-        }, function () {
-          $scope.creating = false;
+          $scope.$apply();
         });
       };
 
       $scope.remove = function (user) {
-        $scope.destroying = user.id;
         User.destroy(user.id).then(function () {
-          delete $scope.destroying;
-          $timeout();
-        }, function () {
-          delete $scope.destroying;
+          $scope.$apply();
         });
       };
-
-      $scope.$watch(function () {
-        return User.lastModified();
-      }, function () {
-        $scope.users = User.filter();
-      });
     });
 })();
