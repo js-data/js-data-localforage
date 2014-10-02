05ebe28f4f4671cc186d148b4dfe7cf58bccb52c
Jason Dobry <jason.dobry@gmail.com>
2014-10-01 23:58:56 -0600

    Example update.

--- dist/js-data-localforage.js
+++ dist/js-data-localforage.js
@@ -1,7 +1,7 @@
 /**
 * @author Jason Dobry <jason.dobry@gmail.com>
 * @file js-data-localforage.js
-* @version 0.4.1 - Homepage <http://www.js-data.iojs-data-localforage/>
+* @version 0.4.2 - Homepage <http://www.js-data.iojs-data-localforage/>
 * @copyright (c) 2014 Jason Dobry 
 * @license MIT <https://github.com/js-data/js-data-localforage/blob/master/LICENSE>
 *
